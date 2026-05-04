8ce340a002b96b2594013578731ae5e90f15b851
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-05-04 11:01:09 -0500

    Add sitemap & robots archives

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://pravice-abogadoslaboralistas.vercel.app/sitemap.xml",
+  };
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,44 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://pravice-abogadoslaboralistas.vercel.app";
+
+  const routes = [
+    "",
+    "/blog",
+
+    // BLOGS
+    "/blog/acoso-laboral-colombia",
+    "/blog/contrato-servicios-vs-laboral-colombia",
+    "/blog/demanda-laboral-colombia",
+    "/blog/despido-injustificado-colombia",
+    "/blog/despido-justa-causa-colombia",
+    "/blog/horas-extras-colombia",
+    "/blog/indemnizacion-despido-colombia",
+    "/blog/liquidacion-laboral-colombia",
+    "/blog/periodo-prueba-colombia",
+    "/blog/renuncia-laboral-colombia",
+    "/blog/tipos-contrato-laboral-colombia",
+    "/blog/vacaciones-laborales-colombia",
+
+    // SERVICIOS
+    "/servicios/demanda-laboral",
+    "/servicios/despidos-laborales",
+    "/servicios/no-pago-salario",
+    "/servicios/seguridad-social",
+    "/servicios/liquidacion-laboral",
+    "/servicios/accidentes-laborales",
+    "/servicios/acoso-laboral",
+    "/servicios/defensa-judicial-laboral",
+    "/servicios/investigaciones-internas",
+    "/servicios/negociaciones-colectivas",
+    "/servicios/proteccion-datos-laborales",
+  ];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: route === "" ? 1 : 0.8,
+  }));
+}
